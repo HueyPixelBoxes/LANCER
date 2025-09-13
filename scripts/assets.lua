f5882cdd536d59e05a5c94ccf2b6df896e593241
Phạm Huy <huyphamquang2003@gmail.com>
2025-09-13 13:45:39 +0700

    Add blackbeard animation

---
+++ scripts/assets.lua
@@ -0,0 +1,37 @@
+local resourcePath = mod_loader.mods[modApi.currentMod].resourcePath
+
+
+local mechPath = resourcePath.."img/mechs/"
+local weaponPath = resourcePath.."img/weapons"
+--Mechs
+local files = {
+	"blackbeard.png",
+	"blackbeard_a.png",
+	"blackbeard_w.png",
+	"blackbeard_w_broken.png",
+	"blackbeard_broken.png",
+	"blackbeard_ns.png",
+	"blackbeard_h.png"
+}
+-- iterate our files and add the assets so the game can find them.
+for _, file in ipairs(files) do
+	modApi:appendAsset("img/units/player/lancer_"..file, mechPath .. file)
+end
+-- create animations for our mech with our imported files.
+-- note how the animations starts searching from /img/
+local a = ANIMS
+a.Blackbeard =			a.MechUnit:new{Image = "units/player/lancer_blackbeard.png", PosX = -24, PosY = -12}
+a.Blackbearda =			a.MechUnit:new{Image = "units/player/lancer_blackbeard_a.png", PosX = -25, PosY = -12, NumFrames = 4 }
+a.Blackbeardw =			a.MechUnit:new{Image = "units/player/lancer_blackbeard_w.png", PosX = -21, PosY = -6 }
+a.Blackbeard_broken =	a.MechUnit:new{Image = "units/player/lancer_blackbeard_broken.png", PosX = -16, PosY = -12}
+a.Blackbeardw_broken =	a.MechUnit:new{Image = "units/player/lancer_blackbeard_w_broken.png", PosX = -17, PosY = -8}
+a.Blackbeard_ns =		a.MechIcon:new{Image = "units/player/lancer_blackbeard_ns.png"}
+
+--deploy are in img/units/mission
+
+--Weapons - the first path must have full path,included resPath part
+modApi:appendAsset("img/weapons/assault_grapple.png", weaponPath.."/assault_grapple.png")
+modApi:appendAsset("img/weapons/bunker_buster.png", weaponPath.."/bunker_buster.png")
+modApi:appendAsset("img/weapons/whitewash_cleaner.png", weaponPath.."/whitewash_cleaner.png")
+modApi:appendAsset("img/weapons/instant_barricade.png",weaponPath.."/instant_barricade.png")
+modApi:appendAsset("img/weapons/subaltern_squad.png", weaponPath.."/subaltern_squad.png")
--- scripts/init.lua
+++ scripts/init.lua
@@ -1,7 +1,7 @@
 local description = "LANCER-inspired mech squads."
 
 local mod = {
-    id = "Con_Gen_LANCERWeapons",
+    id = "LANCER_Weapons",
     name = "LANCER Weapons",
     version = "0.1",
     modApiVersion = "2.9.2",
@@ -12,16 +12,35 @@ local mod = {
 
 -- load .lua files in the path --
 function mod:init()
+    --Add image and animation assets
+    require(self.scriptPath .. "assets")
+    --Add pawns
     require(self.scriptPath.."pawns")
+    --Add weapons 
     require(self.scriptPath.."weapons/ips_n_blackbeard")
     require(self.scriptPath.."weapons/ips_n_drake")
     require(self.scriptPath.."weapons/ips_n_kidd")
     require(self.scriptPath.."weapons/ips_n_kidd_deploy")
+    --Shop
+    modApi:addWeaponDrop("Prime_SwordGrapple")
+	modApi:addWeaponDrop("Brute_Trenchgun")
+	modApi:addWeaponDrop("Deploy_ForgeShot")
+	modApi:addWeaponDrop("Cleaner")
 end
 
 --for what happened when mod are loaded--
 function mod:load(options, version)
-    modApi:addSquad({"IPS Northstar","BlackBeard","Drake","Kidd"},"IPS_N Squad","IPS-Northstar mechs.",self.resourcePath.."/mod_icon.png")
+    modApi:addSquad(
+    {
+        id = "LANCER_Weapons",
+        "IPS Northstar",
+        "BlackBeard",
+        "Drake",
+        "Kidd"
+    },
+    "IPS Northstar",
+    "A squad of mechanized chassis that focus on building blockades to protect critical infrastructures and limited Vek's movements. Seasoned commanders will pay attention to mechs position to not lock themselves into a corner.",
+    self.resourcePath.."img/squad_icon.png")
 end
 
 return mod
