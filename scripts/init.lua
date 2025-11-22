90def48205978725708cdf1318b25b5d774ae0a1
Phạm Huy <huyphamquang2003@gmail.com>
2025-11-22 15:45:49 +0700

    Remove SSC weapon from loot pool

--- scripts/init.lua
+++ scripts/init.lua
@@ -15,42 +15,42 @@ function mod:init()
     --Add image and animation assets
     require(self.scriptPath .. "assets")
     --Add pawns
-    require(self.scriptPath.."pawns")
-    --Add weapons 
-    require(self.scriptPath.."weapons/ips_n_blackbeard")
-    require(self.scriptPath.."weapons/ips_n_drake")
-    require(self.scriptPath.."weapons/ips_n_kidd")
-    require(self.scriptPath.."weapons/ips_n_kidd_deploy")
+    require(self.scriptPath .. "pawns")
+    --Add weapons
+    require(self.scriptPath .. "weapons/ips_n_blackbeard")
+    require(self.scriptPath .. "weapons/ips_n_drake")
+    require(self.scriptPath .. "weapons/ips_n_kidd")
+    require(self.scriptPath .. "weapons/ips_n_kidd_deploy")
 
-    require(self.scriptPath.."passive/ssc_dusk_wing_passive")
-    require(self.scriptPath.."weapons/ssc_duskwing")
-    require(self.scriptPath.."weapons/ssc_emperor")
-    require(self.scriptPath.."weapons/ssc_mourningcloak")
+    -- require(self.scriptPath .. "passive/ssc_dusk_wing_passive")
+    -- require(self.scriptPath .. "weapons/ssc_duskwing")
+    -- require(self.scriptPath .. "weapons/ssc_emperor")
+    -- require(self.scriptPath .. "weapons/ssc_mourningcloak")
     --Shop
     modApi:addWeaponDrop("Prime_SwordGrapple")
-	modApi:addWeaponDrop("Brute_Trenchgun")
-	modApi:addWeaponDrop("Deploy_ForgeShot")
-	modApi:addWeaponDrop("Cleaner")
+    modApi:addWeaponDrop("Brute_Trenchgun")
+    modApi:addWeaponDrop("Deploy_ForgeShot")
+    modApi:addWeaponDrop("Cleaner")
 
-    modApi:addWeaponDrop("SSC_Smoke_Immunity")
-    modApi:addWeaponDrop("Rifle_SmokeSide")
-    modApi:addWeaponDrop("Lightning_Bow")
-    modApi:addWeaponDrop("Tele_Sword")
+    -- modApi:addWeaponDrop("SSC_Smoke_Immunity")
+    -- modApi:addWeaponDrop("Rifle_SmokeSide")
+    -- modApi:addWeaponDrop("Lightning_Bow")
+    -- modApi:addWeaponDrop("Tele_Sword")
 end
 
 --for what happened when mod are loaded--
 function mod:load(options, version)
     modApi:addSquad(
-    {
-        id = "LANCER_Weapons",
+        {
+            id = "LANCER_Weapons",
+            "IPS Northstar",
+            "BlackBeard",
+            "Drake",
+            "Kidd"
+        },
         "IPS Northstar",
-        "BlackBeard",
-        "Drake",
-        "Kidd"
-    },
-    "IPS Northstar",
-    "A squad focus on building blockades to protect critical infrastructures and limited Vek's movements. Seasoned commanders will pay attention to mechs position to not lock themselves into a corner.",
-    self.resourcePath.."img/squad_icon_ipsn.png")
+        "A squad focus on building blockades to protect critical infrastructures and limited Vek's movements. Seasoned commanders will pay attention to mechs position to not lock themselves into a corner.",
+        self.resourcePath .. "img/squad_icon_ipsn.png")
 
     -- modApi:addSquad(
     -- {
