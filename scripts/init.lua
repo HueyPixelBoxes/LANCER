93523fc9a429a4e67b60e650e75e7b28a8943d0b
Phạm Huy <huyphamquang2003@gmail.com>
2026-01-21 21:45:29 +0700

    Minor changes

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,7 +3,7 @@ return {
 	id = "lancer_squads_eapons",
 	name = "LANCER's Squads and Weapons",
 	description = "Mods by Convicted Genius.",
-	icon = "scripts/icon.png",
+	icon = "scripts/mod_icon.png",
 	submodFolders = {"mods/"},
 	version = "0.5",
 	init = function() end,
